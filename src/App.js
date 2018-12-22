5b706ff269eb329b1895332c690f7b734f31d217
Pham Ngoc Linh <pnlinh1207@gmail.com>
2018-12-22 20:31:55 +0700

    Hello app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 
+let One = (props) => (
+  <div>First React App - {props.name}</div>
+);
+
+const Two = (props) => (
+    <div>Demo App - {props.name}</div>
+);
+
+const Four = (props) => {
+  let abc = <span>Hello Word</span>;
+
+  return (
+      <div>
+        <span>Four - {abc}</span>
+      </div>
+  );
+};
+
+function Three(props) {
+  return <div>La la la - {props.name}</div>;
+}
+
+function MultipleLines(props) {
+  return (
+      <div>
+        <div>Multiple lines - {props.name}</div>
+        <div>Multiple lines - {props.name}</div>
+      </div>
+  );
+}
+
+class Person extends Component {
+  render() {
+    return (
+        <div>
+          <div>Name: {this.props.name}</div>
+          <div>Age: {this.props.age}</div>
+        </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
-    let One = <div>Again</div>;
-
     return (
       <div className="App">
         <header className="App-header">
@@ -12,9 +52,13 @@ class App extends Component {
             Hello ReactJS
           </p>
 
-          <p>
-            {One}
-          </p>
+            <One name="One"/>
+            <Two name="Two"/>
+            <Three name="Three"/>
+            <Four/>
+            <MultipleLines name="Multiple lines"/>
+            <Person name="Ngọc Lịnh" age="23"/>
+            <Person name="Ai đó" age="20"/>
         </header>
       </div>
     );
