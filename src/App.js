4d1ec3bb9e312fbbb89f08ce56e22defe1423977
Pham Ngoc Linh <pnlinh1207@gmail.com>
2018-12-23 11:29:00 +0700

    State

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 let One = (props) => (
-  <div>First React App - {props.name}</div>
+    <div>First React App - {props.name}</div>
 );
 
 const Two = (props) => (
@@ -10,59 +10,88 @@ const Two = (props) => (
 );
 
 const Four = (props) => {
-  let abc = <span>Hello Word</span>;
+    let abc = <span>Hello Word</span>;
 
-  return (
-      <div>
-        <span>Four - {abc}</span>
-      </div>
-  );
+    return (
+        <div>
+            <span>Four - {abc}</span>
+        </div>
+    );
 };
 
 function Three(props) {
-  return <div>La la la - {props.name}</div>;
+    return <div>La la la - {props.name}</div>;
 }
 
 function MultipleLines(props) {
-  return (
-      <div>
-        <div>Multiple lines - {props.name}</div>
-        <div>Multiple lines - {props.name}</div>
-      </div>
-  );
+    return (
+        <div>
+            <div>Multiple lines - {props.name}</div>
+            <div>Multiple lines - {props.name}</div>
+        </div>
+    );
 }
 
 class Person extends Component {
-  render() {
-    return (
-        <div>
-          <div>Name: {this.props.name}</div>
-          <div>Age: {this.props.age}</div>
-        </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        // Init state
+        this.state = {
+            name: 'Ngoc Linh',
+            job: 'Developer'
+        };
+
+        // Binding
+        this.handleOnChange = this.handleOnChange.bind(this);
+    }
+
+    handleOnChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        console.log(target, name, value);
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" name="name" onChange={this.handleOnChange} value={this.state.name}/>
+
+                <div>Name: {this.state.name}</div>
+                <div>Age: {this.props.age}</div>
+                <div>Job: {this.state.job}</div>
+            </div>
+        );
+    }
 }
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Hello ReactJS
-          </p>
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        Hello ReactJS
+                    </p>
 
-            <One name="One"/>
-            <Two name="Two"/>
-            <Three name="Three"/>
-            <Four/>
-            <MultipleLines name="Multiple lines"/>
-            <Person name="Ngọc Lịnh" age="23"/>
-            <Person name="Ai đó" age="20"/>
-        </header>
-      </div>
-    );
-  }
+                    {/*<One name="One"/>*/}
+                    {/*<Two name="Two"/>*/}
+                    {/*<Three name="Three"/>*/}
+                    {/*<Four/>*/}
+                    {/*<MultipleLines name="Multiple lines"/>*/}
+                    {/*<Person name="Ngọc Lịnh" age="23"/>*/}
+                    {/*<Person name="Ai đó" age="20"/>*/}
+                    <Person age={23}/>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
